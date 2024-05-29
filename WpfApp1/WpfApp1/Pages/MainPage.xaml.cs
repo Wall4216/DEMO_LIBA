9802888d517c9c271e1db716e394c0f277ddba8f
Wall4216 <ilnazamirhanov61@gmail.com>
2024-05-29 19:11:43 +0300

    add project

---
+++ WpfApp1/WpfApp1/Pages/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VariablePartLibrary.Models;
+using VariablePartLibrary.Services;
+
+namespace WpfApp1.Pages
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            DGPlant.ItemsSource = DBService.Instance.GetModelData<Plant>().ToList();
+            CBType.ItemsSource = DBService.Instance.GetModelData<PlantType>().ToList();
+        }
+
+        private void TBSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void CBType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void DGPlant_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var selectedPlant = DGPlant.SelectedItem as Plant;
+            if (selectedPlant != null)
+            { 
+                NavigationService.Navigate(new PlantPage(selectedPlant));
+            }
+        }
+
+        private void Refresh()
+        {
+            string searchText = TBSearch.Text.ToLower();
+            var selectedType = CBType.SelectedItem as PlantType;
+
+            var plants = DBService.Instance.GetModelData<Plant>().ToList();
+            if (string.IsNullOrEmpty(searchText) == false)
+            {
+                plants = plants.Where(x => x.Name.ToLower().Contains(searchText)).ToList();
+            }
+
+            if (selectedType != null)
+            {
+                plants = plants.Where(x => x.PlantTypeId == selectedType.Id).ToList();
+            }
+
+            DGPlant.ItemsSource = plants.ToList();
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/Pages/PlantPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using MessagingToolkit.QRCode.Codec;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VariablePartLibrary.Models;
+
+namespace WpfApp1.Pages
+{
+    /// <summary>
+    /// Interaction logic for PlantPage.xaml
+    /// </summary>
+    public partial class PlantPage : Page
+    {
+        Plant contextPlant;
+        public PlantPage(Plant plant)
+        {
+            InitializeComponent();
+            contextPlant = plant;
+            DataContext = contextPlant;
+            
+            if (plant.PlantTypeId == 1)
+            {
+                MainGrid.Background = Brushes.Green;
+            }
+
+            if (plant.PlantTypeId == 2)
+            {
+                MainGrid.Background = Brushes.Blue;
+            }
+
+            if (plant.PlantTypeId == 3)
+            {
+                MainGrid.Background = Brushes.Aqua;
+            }
+
+            var encoder = new QRCodeEncoder();
+
+            var image = encoder.Encode(plant.Description);
+
+            QrCode.Source = Imaging.CreateBitmapSourceFromHBitmap(image.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, null);
+        }
+    }
+}
